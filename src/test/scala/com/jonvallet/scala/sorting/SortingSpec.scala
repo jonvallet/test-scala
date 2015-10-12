9967f5308e99e0231ae9ef57b22788abab89953f
Jon Vallet <j.vallet@validus-ivc.co.uk>
2015-10-12 15:02:39 +0100

    Add Sorting Spec

---
+++ src/test/scala/com/jonvallet/scala/sorting/SortingSpec.scala
@@ -0,0 +1,14 @@
+package com.jonvallet.scala.sorting
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by jon on 12/10/15.
+ */
+class SortingSpec extends FlatSpec with Matchers {
+
+  "bubbleSort(List(2,9,4,2,1,3)" should "return List(1, 2, 2, 3, 4, 9)" in {
+    bubbleSort(List(2,9,4,2,1,3)) should be (List(1, 2, 2, 3, 4, 9))
+  }
+
+}
