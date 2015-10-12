24f7a9ce080be123fa1988515e68bbbbf4c4972b
Jon Vallet <j.vallet@validus-ivc.co.uk>
2015-10-12 13:26:14 +0100

    Make the sorting function generic

--- src/main/scala/com/jonvallet/scala/sorting/package.scala
+++ src/main/scala/com/jonvallet/scala/sorting/package.scala
@@ -5,12 +5,12 @@ package com.jonvallet.scala
  */
 package object sorting {
 
-  def isSort(list: List[Int]): Boolean = list match {
+  def isSort[A <% Ordered[A]](list: List[A]): Boolean = list match {
     case head :: Nil => true
     case head :: tail => if (head > tail.head) false else isSort(tail)
   }
 
-  def bubbleSort (list: List[Int]): List[Int] = {
+  def bubbleSort[A <% Ordered[A]](list: List[A]): List[A] = {
     if (isSort(list)) list
     else if (list.head > list.tail.head) bubbleSort(list.tail ++ List(list.head))
     else bubbleSort(list.head :: bubbleSort(list.tail))
