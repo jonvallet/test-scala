08fdd9f9d70c3a9e65bf86bf0a406ed88221b883
Jon Vallet <j.vallet@validus-ivc.co.uk>
2016-03-29 11:40:40 +0100

    First spec is done

--- src/main/scala/com/jonvallet/scala/sums/package.scala
+++ src/main/scala/com/jonvallet/scala/sums/package.scala
@@ -4,5 +4,5 @@ package com.jonvallet.scala
   * Created by j.vallet@validus-ivc.co.uk on 29/03/16.
   */
 package object sums {
-  def sums(totals : Map[String, Int])(key: String, total: Int): Map [String, Int] = ???
+  def sums(totals : Map[String, Int])(key: String, total: Int): Map [String, Int] = Map(key -> total) ++ totals
 }
