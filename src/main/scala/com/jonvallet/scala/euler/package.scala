17a2f12891df66856edf97fe746da631ed9840bb
Jon Vallet <j.vallet@validus-ivc.co.uk>
2016-04-07 14:43:31 +0100

    Some isPrime optimizations

--- src/main/scala/com/jonvallet/scala/euler/package.scala
+++ src/main/scala/com/jonvallet/scala/euler/package.scala
@@ -7,7 +7,14 @@ package object euler {
 
   def sum(l: Seq[Int]) = l.fold(0)((a, b) => a + b)
 
-  def isPrime(n: Long): Boolean = primes.takeWhile(p => p*p <= n).forall(n % _ > 0)
+  def isPrime(n: Long): Boolean = {
+    if (n == 1) false
+    else if (n <4) true
+    else if (n % 2 == 0) false
+    else if (n < 9) true
+    else if (n % 3 == 0) false
+    else primes.takeWhile(p => p*p <= n).forall(n % _ > 0)
+  }
 
   lazy val primes: Stream[Long] = 2L #:: Stream.iterate(3L)(_ + 1L).filter(isPrime)
 
